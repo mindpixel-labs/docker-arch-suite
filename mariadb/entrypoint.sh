5be12072fc9d02872696aec8071b72d0121f0036
Unknown <andrzej1_1@o2.pl>
2018-03-18 20:50:31 +0100

    Handle mariadb

--- mariadb/entrypoint.sh
+++ mariadb/entrypoint.sh
@@ -3,6 +3,10 @@
 set -eo pipefail
 shopt -s nullglob
 
+# UID/GID map to unknown user/group, $HOME=/ (the default when no home directory is defined)
+eval $( fixuid )
+# UID/GID now match user/group, $HOME has been set to user's home directory
+
 # if command starts with an option, prepend mysqld
 if [ "${1:0:1}" = '-' ]; then
 	set -- mysqld "$@"
